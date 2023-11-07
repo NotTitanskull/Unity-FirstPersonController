986a8a32979d1416abebbefeeb70d3beb7ffb469
Christian Fenger <fengercs@gmail.com>
2023-11-06 18:55:36 -0600

    Added Sprinting & Optimized code

--- Assets/FirstPersonController.cs
+++ Assets/FirstPersonController.cs
@@ -1,30 +1,37 @@
-using System.Numerics;
 using UnityEngine;
 
 public class FirstPersonController : MonoBehaviour
 {
-    public bool CanMove { get; private set; } = true;
+    [Header("Functional Options")] [SerializeField]
+    private bool canSprint = true;
 
-    [Header("Movement Parameters")]
-    [SerializeField] private float walkSpeed = 3.0f;
+    [Header("Controls")] [SerializeField] private KeyCode sprintKey = KeyCode.LeftShift;
+
+    [Header("Movement Parameters")] [SerializeField]
+    private float walkSpeed = 3.0f;
+
+    [SerializeField] private float sprintSpeed = 6.0f;
     [SerializeField] private float gravity = 30.0f;
 
-    [Header("Look Parameter")]
-    [SerializeField, Range(1, 10)] private float lookSpeedX = 2.0f;
+    [Header("Look Parameter")] [SerializeField, Range(1, 10)]
+    private float lookSpeedX = 2.0f;
+
     [SerializeField, Range(1, 10)] private float lookSpeedY = 2.0f;
     [SerializeField, Range(1, 180)] private float upperLookLimit = 80.0f;
     [SerializeField, Range(1, 180)] private float lowerLookLimit = 80.0f;
+    private CharacterController characterController;
+    private Vector2 currentInput;
+
+    private Vector3 moveDirection;
 
     private Camera playerCamera;
-    private CharacterController characterController;
 
-    private UnityEngine.Vector3 moveDirection;
-    private UnityEngine.Vector2 currentInput;
-
-    private float rotationX = 0;
+    private float rotationX;
+    private bool CanMove { get; set; } = true;
+    private bool IsSprinting => canSprint && Input.GetKey(sprintKey);
 
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         playerCamera = GetComponentInChildren<Camera>();
         characterController = GetComponent<CharacterController>();
@@ -33,23 +40,23 @@ public class FirstPersonController : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        if (CanMove)
-        {
-            HandleMovementInput();
-            HandleMouseLook();
+        if (!CanMove) return;
+        HandleMovementInput();
+        HandleMouseLook();
 
-            ApplyFinalMovements();
-        }
+        ApplyFinalMovements();
     }
 
     private void HandleMovementInput()
     {
-        currentInput = new UnityEngine.Vector2(walkSpeed * Input.GetAxis("Vertical"), walkSpeed * Input.GetAxis("Horizontal"));
+        currentInput = new Vector2((IsSprinting ? sprintSpeed : walkSpeed) * Input.GetAxis("Vertical"),
+            (IsSprinting ? sprintSpeed : walkSpeed) * Input.GetAxis("Horizontal"));
 
-        float moveDirectionY = moveDirection.y;
-        moveDirection = (transform.TransformDirection(UnityEngine.Vector3.forward) * currentInput.x) + (transform.TransformDirection(UnityEngine.Vector3.right) * currentInput.y);
+        var moveDirectionY = moveDirection.y;
+        moveDirection = (transform.TransformDirection(Vector3.forward) * currentInput.x) +
+                        (transform.TransformDirection(Vector3.right) * currentInput.y);
         moveDirection.y = moveDirectionY;
     }
 
@@ -57,13 +64,13 @@ public class FirstPersonController : MonoBehaviour
     {
         rotationX -= Input.GetAxis("Mouse Y") * lookSpeedY;
         rotationX = Mathf.Clamp(rotationX, -upperLookLimit, lowerLookLimit);
-        playerCamera.transform.localRotation = UnityEngine.Quaternion.Euler(rotationX, 0, 0);
-        transform.rotation *= UnityEngine.Quaternion.Euler(0, Input.GetAxis("Mouse X") * lookSpeedX, 0);
+        playerCamera.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
+        transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * lookSpeedX, 0);
     }
 
     private void ApplyFinalMovements()
     {
-        if(!characterController.isGrounded)
+        if (!characterController.isGrounded)
             moveDirection.y -= gravity * Time.deltaTime;
 
         characterController.Move(moveDirection * Time.deltaTime);
