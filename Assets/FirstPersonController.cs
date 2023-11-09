5bc9968b6a3c3e7d0737e9c2d022d5f1b775e688
Christian Fenger <fengercs@gmail.com>
2023-11-09 13:48:13 -0600

    First Commit

--- Assets/FirstPersonController.cs
+++ Assets/FirstPersonController.cs
@@ -2,6 +2,22 @@ using UnityEngine;
 
 public class FirstPersonController : MonoBehaviour
 {
+    [Header("Movement Parameters")] [SerializeField]
+    private float walkSpeed = 3.0f;
+
+    [SerializeField] private float gravity = 30.0f;
+
+    [Header("Look Parameters")] [SerializeField, Range(1, 10)]
+    private float horizontalLookSpeed = 2.0f;
+
+    [SerializeField, Range(1, 10)] private float verticalLookSpeed = 2.0f;
+    [SerializeField, Range(1, 180)] private float maxVerticalLookAngle = 80.0f;
+    [SerializeField, Range(1, 180)] private float minVerticalLookAngle = 80.0f;
+    private CharacterController characterController;
+
+    private Camera playerCamera;
+    public bool CanMove { get; private set; } = true;
+
     // Start is called before the first frame update
     void Start()
     {
