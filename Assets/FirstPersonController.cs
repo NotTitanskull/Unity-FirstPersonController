bf79da4bd789b351bf911443f75c9c53d2fb3139
Christian Fenger <fengercs@gmail.com>
2023-11-09 14:34:27 -0600

    Update

--- Assets/FirstPersonController.cs
+++ Assets/FirstPersonController.cs
@@ -14,17 +14,59 @@ public class FirstPersonController : MonoBehaviour
     [SerializeField, Range(1, 180)] private float maxVerticalLookAngle = 80.0f;
     [SerializeField, Range(1, 180)] private float minVerticalLookAngle = 80.0f;
     private CharacterController characterController;
+    private Vector2 currentInput;
+
+    private Vector3 moveDirection;
 
     private Camera playerCamera;
-    public bool CanMove { get; private set; } = true;
+
+    private float rotationX = 0;
+    public bool IsMovementEnabled { get; private set; } = true;
 
     // Start is called before the first frame update
     void Start()
     {
+        playerCamera = GetComponentInChildren<Camera>();
+        characterController = GetComponent<CharacterController>();
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (IsMovementEnabled)
+        {
+            HandleMovementInput();
+            HandleMouseLook();
+
+            ApplyFinalMovements();
+        }
+    }
+
+    private void HandleMovementInput()
+    {
+        currentInput = new Vector2(walkSpeed * Input.GetAxis("Vertical"), walkSpeed * Input.GetAxis("Horizontal"));
+
+        float moveDirectionY = moveDirection.y;
+        moveDirection = (transform.TransformDirection(Vector3.forward) * currentInput.x) +
+                        (transform.TransformDirection(Vector3.right) * currentInput.y);
+        moveDirection.y = moveDirectionY;
+    }
+
+    private void HandleMouseLook()
+    {
+        rotationX -= Input.GetAxis("Mouse Y") * verticalLookSpeed;
+        rotationX = Mathf.Clamp(rotationX, -maxVerticalLookAngle, minVerticalLookAngle);
+        playerCamera.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
+        transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * horizontalLookSpeed, 0);
+    }
+
+    private void ApplyFinalMovements()
+    {
+        if (!characterController.isGrounded)
+            moveDirection.y -= gravity * Time.deltaTime;
+
+        characterController.Move(moveDirection * Time.deltaTime);
     }
 }
