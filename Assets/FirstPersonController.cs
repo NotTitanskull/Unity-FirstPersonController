d47703a6c86404cf1b2c18ef5fbab5afa534a7c8
Christian Fenger <fengercs@gmail.com>
2023-11-15 15:19:38 -0600

    Implemented Jumping mechanics and optimized code

--- Assets/FirstPersonController.cs
+++ Assets/FirstPersonController.cs
@@ -5,76 +5,92 @@ public class FirstPersonController : MonoBehaviour
     [Header("Functional Options")] [SerializeField]
     private bool canSprint = true;
 
+    [SerializeField] private bool canJump = true;
+
     [Header("Controls")] [SerializeField] private KeyCode sprintKey = KeyCode.LeftShift;
+
+    [SerializeField] private KeyCode jumpKey = KeyCode.Space;
 
     [Header("Movement Parameters")] [SerializeField]
     private float walkSpeed = 3.0f;
 
     [SerializeField] private float sprintSpeed = 6.0f;
-    [SerializeField] private float gravity = 30.0f;
 
     [Header("Look Parameter")] [SerializeField, Range(1, 10)]
-    private float lookSpeedX = 2.0f;
+    private float horizontalLookSpeed = 2.0f;
 
-    [SerializeField, Range(1, 10)] private float lookSpeedY = 2.0f;
-    [SerializeField, Range(1, 180)] private float upperLookLimit = 80.0f;
-    [SerializeField, Range(1, 180)] private float lowerLookLimit = 80.0f;
-    private CharacterController characterController;
-    private Vector2 currentInput;
+    [SerializeField, Range(1, 10)] private float verticalLookSpeed = 2.0f;
+    [SerializeField, Range(1, 180)] private float maxVerticalLookAngle = 80.0f;
+    [SerializeField, Range(1, 180)] private float minVerticalLookAngle = 80.0f;
 
-    private Vector3 moveDirection;
+    [Header("Jumping Parameters")] [SerializeField]
+    private float jumpForce = 8.0f;
 
-    private Camera playerCamera;
+    [SerializeField] private float gravity = 30.0f;
+    private CharacterController _characterController;
+    private Vector2 _currentInput;
 
-    private float rotationX = 0;
-    public bool CanMove { get; private set; } = true;
+    private Vector3 _moveDirection;
+
+    private Camera _playerCamera;
+
+    private float _rotationX;
+    private bool CanMove { get; set; } = true;
     private bool IsSprinting => canSprint && Input.GetKey(sprintKey);
+    private bool ShouldJump => Input.GetKeyDown(jumpKey) && _characterController.isGrounded;
 
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        playerCamera = GetComponentInChildren<Camera>();
-        characterController = GetComponent<CharacterController>();
+        _playerCamera = GetComponentInChildren<Camera>();
+        _characterController = GetComponent<CharacterController>();
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        if (CanMove)
-        {
-            HandleMovementInput();
-            HandleMouseLook();
+        if (!CanMove) return;
+        HandleMovementInput();
+        HandleMouseLook();
 
-            ApplyFinalMovements();
-        }
+        if (canJump)
+            HandleJump();
+
+        ApplyFinalMovements();
     }
 
     private void HandleMovementInput()
     {
-        currentInput = new Vector2((IsSprinting ? sprintSpeed : walkSpeed) * Input.GetAxis("Vertical"),
+        _currentInput = new Vector2((IsSprinting ? sprintSpeed : walkSpeed) * Input.GetAxis("Vertical"),
             (IsSprinting ? sprintSpeed : walkSpeed) * Input.GetAxis("Horizontal"));
 
-        float moveDirectionY = moveDirection.y;
-        moveDirection = (transform.TransformDirection(Vector3.forward) * currentInput.x) +
-                        (transform.TransformDirection(Vector3.right) * currentInput.y);
-        moveDirection.y = moveDirectionY;
+        var moveDirectionY = _moveDirection.y;
+        _moveDirection = (transform.TransformDirection(Vector3.forward) * _currentInput.x) +
+                         (transform.TransformDirection(Vector3.right) * _currentInput.y);
+        _moveDirection.y = moveDirectionY;
     }
 
     private void HandleMouseLook()
     {
-        rotationX -= Input.GetAxis("Mouse Y") * lookSpeedY;
-        rotationX = Mathf.Clamp(rotationX, -upperLookLimit, lowerLookLimit);
-        playerCamera.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
-        transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * lookSpeedX, 0);
+        _rotationX -= Input.GetAxis("Mouse Y") * verticalLookSpeed;
+        _rotationX = Mathf.Clamp(_rotationX, -maxVerticalLookAngle, minVerticalLookAngle);
+        _playerCamera.transform.localRotation = Quaternion.Euler(_rotationX, 0, 0);
+        transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * horizontalLookSpeed, 0);
+    }
+
+    private void HandleJump()
+    {
+        if (ShouldJump)
+            _moveDirection.y = jumpForce;
     }
 
     private void ApplyFinalMovements()
     {
-        if (!characterController.isGrounded)
-            moveDirection.y -= gravity * Time.deltaTime;
+        if (!_characterController.isGrounded)
+            _moveDirection.y -= gravity * Time.deltaTime;
 
-        characterController.Move(moveDirection * Time.deltaTime);
+        _characterController.Move(_moveDirection * Time.deltaTime);
     }
 }
